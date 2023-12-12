2ff0baea64150206f9e9497033ee33cfa072d496
Erwin Mintiens <erwin.mintiens@protonmail.com>
2023-12-12 23:08:15 +0100

    Added day6 part 2

--- src/day6_2023.rs
+++ src/day6_2023.rs
@@ -4,13 +4,13 @@ use std::path::Path;
 
 #[derive(Debug)]
 pub struct Race {
-    total_time: u32,
-    distance_to_beat: u32,
+    total_time: u64,
+    distance_to_beat: u64,
 }
 
 impl Race {
-    pub fn calculate_number_of_beating_ways(&self) -> u32 {
-        let mut number_of_non_beating_ways: u32 = 0;
+    pub fn calculate_number_of_beating_ways(&self) -> u64 {
+        let mut number_of_non_beating_ways: u64 = 0;
         for i in 0..self.total_time {
             if i * (self.total_time - i) < self.distance_to_beat {
                 number_of_non_beating_ways += 1;
@@ -18,7 +18,7 @@ impl Race {
                 break;
             }
         }
-        self.total_time - 2 * number_of_non_beating_ways + 1
+        self.total_time - 2_u64 * number_of_non_beating_ways + 1
     }
 }
 
@@ -26,8 +26,8 @@ pub fn read_file_to_races(path: &str) -> io::Result<Vec<Race>> {
     let path = Path::new(path);
     let file = File::open(&path)?;
 
-    let mut time_values: Vec<u32> = Vec::new();
-    let mut distance_values: Vec<u32> = Vec::new();
+    let mut time_values: Vec<u64> = Vec::new();
+    let mut distance_values: Vec<u64> = Vec::new();
 
     let mut races: Vec<Race> = Vec::new();
 
@@ -38,12 +38,12 @@ pub fn read_file_to_races(path: &str) -> io::Result<Vec<Race>> {
 
         if words[0] == "Time:" {
             for i in 1..5 {
-                time_values.push(words[i].parse::<u32>().unwrap());
+                time_values.push(words[i].parse::<u64>().unwrap());
             }
         }
         if words[0] == "Distance:" {
             for i in 1..5 {
-                distance_values.push(words[i].parse::<u32>().unwrap());
+                distance_values.push(words[i].parse::<u64>().unwrap());
             }
         }
     }
@@ -56,4 +56,31 @@ pub fn read_file_to_races(path: &str) -> io::Result<Vec<Race>> {
     }
 
     Ok(races)
+}
+
+pub fn read_file_to_one_race(path: &str) -> io::Result<Race> {
+    let path = Path::new(path);
+    let file = File::open(&path)?;
+
+    let mut time_result: String = String::new();
+    let mut distance_result: String = String::new();
+
+    for line in io::BufReader::new(file).lines() {
+        let line = line?;
+
+        let words: Vec<&str> = line.split_whitespace().collect();
+        if words[0] == "Time:" {
+            for i in 1..5 {
+                time_result.push_str(words[i]);
+            }
+        } else if words[0] == "Distance:" {
+            for i in 1..5 {
+                distance_result.push_str(words[i]);
+            }
+        }
+    }
+    Ok(Race {
+        total_time: time_result.parse::<u64>().unwrap(),
+        distance_to_beat: distance_result.parse::<u64>().unwrap(),
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 pub mod day6_2023;
-use day6_2023::read_file_to_races;
+use day6_2023::{read_file_to_one_race, read_file_to_races};
 
 fn main() {
     day6();
@@ -15,7 +15,7 @@ fn day6_part1() {
         panic!("Failed to read times and distances");
     };
 
-    let mut result: u32 = 1;
+    let mut result: u64 = 1;
     for race in races {
         result = result * race.calculate_number_of_beating_ways();
     }
@@ -23,4 +23,10 @@ fn day6_part1() {
     println!("Result: {:?}", result);
 }
 
-fn day6_part2() {}
+fn day6_part2() {
+    let Ok(race) = read_file_to_one_race("input_files/input_day_6.txt") else {
+        panic!("Failed to read to race!")
+    };
+    let result = race.calculate_number_of_beating_ways();
+    println!("Result part 2: {:?}", result);
+}
